c230c7ac2172fd5262504cb3ce80221f71633e7f
Thedume <jjojin7168@gmail.com>
2026-09-08 16:39:28 +0900

    전역 예외처리 수정 : 404, 409에 메시지 추가 및 예외처리 사용했던 코드에 적용

--- src/main/java/com/gamebasic/game/service/GameService.java
+++ src/main/java/com/gamebasic/game/service/GameService.java
@@ -1,5 +1,7 @@
 package com.gamebasic.game.service;
 
+import com.gamebasic.common.exception.GameFinishedException;
+import com.gamebasic.common.exception.GameNotFoundException;
 import com.gamebasic.game.dto.*;
 import com.gamebasic.game.entity.Game;
 import com.gamebasic.game.entity.GameStatus;
@@ -55,7 +57,8 @@ public class GameService {
 
     private Game findGame(Long gameId) {
         return gameRepository.findById(gameId)
-            .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+            .orElseThrow(() -> new GameNotFoundException(gameId)
+            );
     }
 
     @Transactional
@@ -63,10 +66,7 @@ public class GameService {
         Game game = findGame(gameId);
 
         if (game.isFinished()){
-            throw new ResponseStatusException(
-                    HttpStatus.CONFLICT,
-                    "이미 끝난 게임입니다. (id : " + gameId + ")"
-            );
+            throw new GameFinishedException(gameId);
         }
 
         game.updateProgress(
