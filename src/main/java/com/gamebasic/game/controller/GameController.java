8aa07b8dfdf8a80c94a78f5dcda80a4a1ce20f5d
Thedume <jjojin7168@gmail.com>
2026-09-08 15:45:33 +0900

    게임 플레이어 이름 변경 기능 구현

--- src/main/java/com/gamebasic/game/controller/GameController.java
+++ src/main/java/com/gamebasic/game/controller/GameController.java
@@ -1,9 +1,6 @@
 package com.gamebasic.game.controller;
 
-import com.gamebasic.game.dto.CreateRequest;
-import com.gamebasic.game.dto.GameDetailResponse;
-import com.gamebasic.game.dto.GameSummaryResponse;
-import com.gamebasic.game.dto.ProgressRequest;
+import com.gamebasic.game.dto.*;
 import com.gamebasic.game.service.GameService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -42,5 +39,15 @@ public class GameController {
      @Valid @RequestBody ProgressRequest request
     ) {
      return ResponseEntity.ok(gameService.updateProgress(gameId, request));
+    }
+
+    // 플레이어 닉네임 수정
+    @PatchMapping("/games/{gameId}")
+    public ResponseEntity<Void> renameGame(
+            @PathVariable Long gameId,
+            @Valid @RequestBody RenameRequest request
+    ) {
+        gameService.renameGame(gameId, request);
+        return ResponseEntity.noContent().build();
     }
 }
